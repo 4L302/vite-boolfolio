d45226cf93bfa3450937f8d9ffb6089595a2f17b
4L302 <alegarg2002@gmail.com>
2023-06-16 16:31:12 +0200

    app vue

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import axios from "axios";
+import MainComponent from "./components/MainComponent.vue";
+
+export default {
+  'name': "App",
+  data(){
+    return{
+      title: 'Boolfolio',
+      products: [],
+      apiUrl: 'http://127.0.0.1:8000/api/'
+    }
+  },
+  methods: {
+    getData(){
+      axios.get(`${this.apiUrl}/projects`).then((res) => {
+        console.log(res);
+      })    
+    }
+  },
+  mounted(){
+    this.data();
+  }
+}
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+
+</style>
